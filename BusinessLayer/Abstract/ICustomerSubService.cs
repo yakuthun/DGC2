a63c50656c598c10b4456d69a124e5fe4b938a14
Egemen Karataş <segemen.karatas@gmail.com>
2022-02-08 15:56:23 +0300

    tedarikçi

---
+++ BusinessLayer/Abstract/ICustomerSubService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface ICustomerSubService
+    {
+        List<SubCustomer> GetList();
+        void SubCustomerAdd(SubCustomer subcustomer);
+        SubCustomer GetByID(int id);
+        void SubCustomerDelete(SubCustomer subcustomer);
+        void SubCustomerUpdate(SubCustomer subcustomer);
+    }
+}
