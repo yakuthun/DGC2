4f2266fc6b654b2f6e9394cbf5778a0d8f09cd16
yakuthun <yakuthun@gmail.com>
2022-02-08 10:24:03 +0300

    Yonetici acildi. crud islemleri tamamlandi. adminLayout duzenlendi

---
+++ DGC2/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DGC2.Controllers
+{
+    public class AdminController : Controller
+    {
+        AdminManager am = new AdminManager(new EfAdminDal());
+        public ActionResult Index()
+        {
+            var adminvalues = am.GetList();
+            return View(adminvalues);
+        }
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAdmin(Admin p)
+        {
+            am.AdminAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditAdmin(int id)
+        {
+            var adminvalue = am.GetByID(id);
+            return View(adminvalue);
+        }
+        [HttpPost]
+        public ActionResult EditAdmin(Admin p)
+        {
+            am.AdminUpdate(p);
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult DeleteAdmin(int id)
+        {
+            var adminvalue = am.GetByID(id);
+            am.AdminDelete(adminvalue);
+            return RedirectToAction("Index");
+        }
+    }
+}
