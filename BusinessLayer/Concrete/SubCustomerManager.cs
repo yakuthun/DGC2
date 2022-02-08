394d0a641e2581bcbf0c85aebba9ea5738f4d668
Egemen Karataş <segemen.karatas@gmail.com>
2022-02-08 19:19:12 +0300

    Son değişiklikler

---
+++ BusinessLayer/Concrete/SubCustomerManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class SubCustomerManager : ISubCustomerService
+    {
+        ISubCustomerDal _subcustomerDal;
+
+        public SubCustomerManager(ISubCustomerDal subcustomerDal)
+        {
+            _subcustomerDal = subcustomerDal;
+        }
+
+        public void DriverAdd(Driver driver)
+        {
+            _subcustomerDal.Insert(driver);
+        }
+
+        public void DriverDelete(Driver driver)
+        {
+            _subcustomerDal.Update(driver);
+        }
+
+        public void DriverUpdate(Driver driver)
+        {
+            _subcustomerDal.Update(driver);
+        }
+
+        public Driver GetByID(int id)
+        {
+            return _subcustomerDal.Get(x => x.DriverID == id);
+        }
+
+        public List<Driver> GetList()
+        {
+            return _subcustomerDal.List();
+        }
+    }
+}
---
+++ DGC2/Controllers/SubCustomerController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DGC2.Controllers
+{
+    public class SubCustomerController : Controller
+    {
+        // GET: SubCustomer
+        SubCustomerManager scm = new SubCustomerManager(new EfSubCustomerDal());
+        public ActionResult Index()
+        {
+            var Subcustomervalues = scm.GetList();
+            return View(Subcustomervalues);
+        }
+        [HttpGet]
+        public ActionResult AddDriver()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddDriver(Driver p)
+        {
+            scm.DriverAdd(p);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EditDriver(int id)
+        {
+            var drivervalue = scm.GetByID(id);
+            return View(drivervalue);
+        }
+        [HttpPost]
+        public ActionResult EditDriver(Driver p)
+        {
+            scm.DriverUpdate(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteDriver(int id)
+        {
+            var drivervalue = scm.GetByID(id);
+            drivervalue.DriverStatus = false;
+            scm.DriverUpdate(drivervalue);
+            return RedirectToAction("Index");
+        }
+    }
+}
